e30a6af04c8d4d05ef4abd8b6dc0cc4d813c22f1
aravind <aravindsanalkumar@gmail.com>
2024-09-05 13:46:24 +0530

    fetch api working

--- script.js
+++ script.js
@@ -2,15 +2,16 @@
 
 const dogImage = document.getElementById('dogImage');
 
-console.log('start');
-fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response =>  response.json())
-    .then(json => {
-        console.log(json.message);
-        dogImage.innerHTML = `<img src="${json.message}" alt="" height=300 width=300>`
-    });
-console.log('end');
-
+const randomDog = () => {
+    console.log('start');
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response =>  response.json())
+        .then(json => {
+            console.log(json.message);
+            dogImage.innerHTML = `<img src="${json.message}" alt="" height=300 width=300>`
+        });
+    console.log('end');
+}
 
 console.log(fetch('https://dog.ceo/api/breeds/image/random'));
 
